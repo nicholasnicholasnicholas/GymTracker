3f7be8247001b708f69b4ab9240dfd17decc5ea4
jerrrico <jericoavila@csu.fullerton.edu>
2025-10-01 13:14:59 -0700

    addded users to db

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -8,40 +8,10 @@ namespace GymTracker.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options) { }
         public DbSet<User> Users { get; set; }
-        public DbSet<Exercise> Exercises { get; set; }
-        public DbSet<Muscle> Muscles { get; set; }
-        public DbSet<ExerciseMuscle> ExerciseMuscles { get; set; }
-
-        public DbSet<WorkoutSession> WorkoutSessions { get; set; }
-        public DbSet<WorkoutEntry> WorkoutEntries { get; set; }
-        public DbSet<WorkoutSet> WorkoutSets { get; set; }
-
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-
-            modelBuilder.Entity<ExerciseMuscle>()
-                .HasKey(em => new { em.ExerciseId, em.MuscleId });
-
-            modelBuilder.Entity<ExerciseMuscle>()
-                .HasOne(em => em.Exercise)
-                .WithMany(e => e.ExerciseMuscles) // fixed
-                .HasForeignKey(em => em.ExerciseId);
-
-            modelBuilder.Entity<ExerciseMuscle>()
-                .HasOne(em => em.Muscle)
-                .WithMany(m => m.ExerciseMuscles) // fixed
-                .HasForeignKey(em => em.MuscleId);
-
-            // Seed Muscles
-            Muscle.Seed(modelBuilder);
-
-            // Seed excercises
-            Exercise.Seed(modelBuilder);
-
-            // Seed excerciseMuscle
-            ExerciseMuscle.Seed(modelBuilder);
         }
     }
 }
