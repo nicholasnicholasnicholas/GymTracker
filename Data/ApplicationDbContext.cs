bae040d99e652fcf8c5948307ac3c89741bd13b4
nicholasnicholasnicholas <nicholas.p.legaspi@gmail.com>
2025-10-27 13:25:53 -0700

    Bug fixes to main

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -9,20 +9,12 @@ namespace GymTracker.Data
             : base(options) { }
         public DbSet<User> Users { get; set; }
 
-        public DbSet<WorkoutSession> WorkoutSessions { get; set; }
-        public DbSet<WorkoutEntry> WorkoutEntries { get; set; }
-        public DbSet<Set> Sets { get; set; }
-        public DbSet<Exercise> Exercises { get; set; }
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
         }
     }
 }
-
-
-
 
 
 /*
