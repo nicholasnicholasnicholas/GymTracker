ba3eb002f97cf34e07f2aa8769e87f7998a1f9ff
jerrrico <jericoavila@csu.fullerton.edu>
2025-10-29 11:58:31 -0700

    added user session service.cs

---
+++ Services/UserSessionService.cs
@@ -0,0 +1,21 @@
+namespace GymTracker.Services
+{
+    public class UserSessionService
+    {
+        public bool IsLoggedIn { get; private set; } = false;
+        public string Username { get; private set; } = string.Empty;
+
+        public void Login(string username)
+        {
+            IsLoggedIn = true;
+            Username = username;
+        }
+
+        public void Logout()
+        {
+            IsLoggedIn = false;
+            Username = string.Empty;
+        }
+    }
+}
+
